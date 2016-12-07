d0e6421c7092c1d581f7f18bfed5f8f4ec673dcd
Sargis Khlopuzyan <sargiskh@energizeglobal.com>
2016-12-07 16:42:45 +0400

    chapter 31

---
+++ app/src/main/java/com/example/sargiskh/locatr/GalleryItem.java
@@ -0,0 +1,55 @@
+package com.example.sargiskh.locatr;
+
+import android.net.Uri;
+
+/**
+ * Created by sargiskh on 12/1/2016.
+ */
+
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private String mOwner;
+
+
+    public void setCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public void setId(String mId) {
+        this.mId = mId;
+    }
+
+    public void setUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+}
